65269507e832661fffcf07c8691a999a17da19e1
pl245851 <70595860+pl245851@users.noreply.github.com>
2020-10-29 17:45:47 -0400

    Add files via upload

---
+++ lava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lava {	
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int cases = scanner.nextInt();
+		for(int i = 0; i<cases; i++) {
+			int length = scanner.nextInt();
+			int width = scanner.nextInt();
+			int dist = scanner.nextInt();
+			
+		}
+
+	}
+
+}
