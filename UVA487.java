1baa8f1f29097a87d50e42a55f4f5d05a819d4dd
pl245851 <70595860+pl245851@users.noreply.github.com>
2021-02-09 17:42:11 -0500

    Add files via upload

---
+++ UVA487.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class UVA487 {
+	static int movex[] = {1,1,1,0,0,-1,-1,-1};
+	static int movey[] = {1,0,-1,1,-1,1,0,-1};
+	static char map[][];
+	static TreeSet<String> set;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int cases = scanner.nextInt();
+		for(int c = 0; c< cases; c++) {
+			set = new TreeSet<>();
+			int length = scanner.nextInt();
+			map = new char[length][length];
+			for(int i = 0; i<length; i++) {
+				String line = scanner.next();
+				for(int j = 0; j<length;j++) {
+					map[i][j] = line.charAt(j);
+				}
+			}
+			for(int i  =0 ; i< length; i++) {
+				for(int j = 0; j< length; j++) {
+					search("",i,j);
+				}
+			}
+			for(int i = 0; i<set.size();i++) {
+				System.out.printf("%s%n", set.pollFirst());
+			}
+			System.out.printf("%n");
+		}
+
+	}
+	public static void search(String current,int x,int y) {
+		if(check(current)) {
+			for(int i = 0; i< 8; i++) {
+				if(x+movex[i]>=0&&x+movex[i]<map.length&&y+movey[i]>=0&&y+movey[i]<map.length) {
+					search(current+map[x+movex[i]][y+movey[i]],x+movex[i] ,y+movey[i]);
+				}
+			}
+		}
+	}
+	
+	
+	public static boolean check(String word) {
+		char[] chek = word.toCharArray();
+		boolean pass = true;
+		for(int i = 1; i<chek.length&&chek.length>1; i++) {
+			if(chek[i]<=chek[i-1]) {
+				pass = false;
+			}
+		}
+		if(pass&&word.length()>2) {
+			set.add(word);
+		}
+		return pass;
+	}
+
+}
